6836cc9c7ddfec772ddea6e6fdb707044ad3c4ec
keerthig12 <keerthimg02@gmail.com>
2024-10-18 18:11:32 +0530

    method file

---
+++ keerthi.java
@@ -0,0 +1,35 @@
+public class keerthi{
+ 
+      public static void main (String[] args){
+
+             goodMorning();
+             goodMorning();
+             goodMorning();
+
+ 
+               country();
+               country();
+               country();
+
+          addvalue();
+          addvalue();
+
+ }
+
+       public static void goodMorning(){
+           System.out.println("say good morning to me");
+ 
+       }
+
+       public static void country(){
+           System.out.println("India is a beautiful country");
+       }
+
+
+       public static void addvalue(){
+            int a =  12;
+            int b = 13;
+           System.out.println(a + b);
+        
+       }
+}
